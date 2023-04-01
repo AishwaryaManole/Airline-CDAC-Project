62aa99c114a2046a281a914067497a88c4510f7f
maishwarya <patilaishwarya121@gmail.com>
2023-04-01 14:57:54 +0530

    Added Airline Ticket Reservation System Project

---
+++ Airline-backend/src/main/java/com/project/Airline_Ticket_Booking_System/AirlineTicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.Airline_Ticket_Booking_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineTicketBookingSystemApplication.class, args);
+	}
+
+}
